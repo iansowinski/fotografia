dbaebc5d3dbfba404906e44f0ebe2855c96a87c4
Ian Sowinski <sowinj@gmail.com>
2017-02-07 11:29:30 +0100

    .gitignore

--- server.rb
+++ server.rb
@@ -6,7 +6,7 @@ include OpenCV
 $face_checker = false
 
 Thread.new do
-    tcp_server = TCPServer.new('192.168.0.100', 4567)
+    tcp_server = TCPServer.new('192.168.1.3', 4567)
     srv = tcp_server.accept
     while true 
         srv.puts "#{$face_checker}"
@@ -15,15 +15,15 @@ end
 
 data = 'model.xml'
 detector = CvHaarClassifierCascade::load(data)
+color = CvColor::Yellow
 capture = OpenCV::CvCapture.open
 window = GUI::Window.new('Face detection')
 while true do
   $face_checker = false
   image = capture.query
   detector.detect_objects(image).each do |region|
-    $face_checker = true
-    color = CvColor::Blue
-    image.rectangle! region.top_left, region.bottom_right, :color => color
+    $face_checker = true   
+    image.rectangle! region.top_left, region.bottom_right, :color => color, :thickness => 3
   end
   window.show(image)
   GUI::wait_key(1)
--- test_client.rb
+++
@@ -1,8 +0,0 @@
-require 'socket'
-
-host = '192.168.0.100'
-port = 4567
-@server = TCPSocket.open host, port
-while true do
-   print @server.gets.chomp
-end
